968734c25a2c9d61a881f6154dbf8543dcece0e1
unknown <mohamedrefat12345@gmail.com>
2025-01-02 17:47:51 +0200

    Creating homeScreen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
+import 'package:e_commerce/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-void main(){
-  runApp(
-    const MyApp()
-  );
+
+void main() {
+  runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Text('Hello World'),
-      ),
+      home: HomeScreen(),
     );
   }
 }
