d579685f08a317dddf9a0c7bae38a982202f6093
unknown <mohamedrefat12345@gmail.com>
2025-01-02 23:27:09 +0200

    finish product item widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,29 @@
+import 'package:e_commerce/models/product_model.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce/widgets/product_item.dart';
-class HomeScreen extends StatelessWidget{
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('E-commerce'),
+      ),
+      body: const ProductItem(
+        product: Product(
+            category: 'men\'s clothing',
+            description:
+                'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            id: '1',
+            image:
+                'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+            price: '109.95',
+            rating: {
+              'rate':3.9,
+              'count':90,
+            },
+            title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops'),
       ),
     );
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,15 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:e_commerce/models/product_model.dart';
 
-class ProductItem extends StatelessWidget{
-  const ProductItem({super.key});
+class ProductItem extends StatelessWidget {
+  const ProductItem({required this.product, super.key});
+  final Product product;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Image.network('https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg'),
-        ],
-      ),
+    return Stack(
+      children: [
+        SizedBox(
+          width: 210,
+          child: Card(
+            shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+                side: const BorderSide(
+                  color: Color.fromARGB(255, 1, 65, 135),
+                  style: BorderStyle.solid,
+                  width: 3,
+                )),
+            color: Colors.white,
+            elevation: 0,
+            clipBehavior: Clip.hardEdge,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  product.image,
+                  width: 170,
+                  height: 170,
+                  fit: BoxFit.fill,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        product.title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 11, 6, 84),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        product.description,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 11, 6, 84),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            product.price,
+                            style: const TextStyle(
+                              fontSize: 19,
+                              color: Color.fromARGB(255, 11, 6, 84),
+                            ),
+                          ),
+                          const Spacer(),
+                          const Text(
+                            '2000 EGP',
+                            style: TextStyle(
+                              decorationColor:
+                                  Color.fromARGB(255, 64, 135, 181),
+                              decoration: TextDecoration.lineThrough,
+                              fontSize: 17,
+                              color: Color.fromARGB(255, 64, 135, 181),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Review (${product.rating['rate']})',
+                            style: const TextStyle(
+                              fontSize: 17,
+                              color: Color.fromARGB(255, 11, 6, 84),
+                            ),
+                          ),
+                          const Icon(
+                            Icons.star_rounded,
+                            color: Color.fromARGB(255, 254, 220, 3),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          right: 12,
+          top: 10,
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.favorite_border,
+                color: Color.fromARGB(255, 1, 65, 135),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 12,
+          bottom: 6,
+          child: Container(
+            width: 45,
+            height: 45,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 0, 64, 135),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
